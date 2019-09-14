101cb24226d7b1125c59354686db3590969339ef
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-14 11:10:43 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,8 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+ bikes = {"honda" => "CRF"}
+ end
 
   
-end
 
 
 def shipping_manifest
